bbf241285a5a1a2e60ad66efedaa0cc7961eb61e
Abir Rakshit <abir.raksh@gmail.com>
2025-01-09 16:57:10 +0600

    practicing middleWare in redux

--- src/redux/store.jsx
+++ src/redux/store.jsx
@@ -1,6 +1,17 @@
-import { createStore } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./rootReducer";
 
-const store = createStore(rootReducer);
+const myLogger = (store) => (next) => (action) => {
+    console.log(`Action: ${JSON.stringify(action)}`);
+    console.log(`Before: ${JSON.stringify(store.getState())}`);
+
+    const upcomingState = [action].reduce(rootReducer, store.getState());
+
+    console.log(`Upcoming State: ${JSON.stringify(upcomingState)}`);
+
+    return next(action);
+}
+
+const store = createStore(rootReducer, applyMiddleware(myLogger));
 
 export default store;
